46a66d578fa7c9a456d07faa038e5d49ce567f63
Yaba Ernesto <yabaernesto@gmail.com>
2024-11-29 19:21:06 +0100

    feat: added validation

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,11 +1,42 @@
+'use client'
+
 import { Facebook, Twitter } from 'lucide-react'
 import { Logo } from '../components/Logo'
 import Link from 'next/link'
 import { Button } from '../components/Button'
 import * as Root from '../components/Form/Input'
 import * as Social from '../components/Form/Social'
+import * as yup from 'yup'
+import { useFormik } from 'formik'
+import { useRouter } from 'next/navigation'
 
 const SignIn = () => {
+  const router = useRouter()
+
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email('O email deve ser válido')
+      .required('O campo email é obrigatório'),
+    password: yup
+      .string()
+      .min(4, 'A senha deve ter no mínimo 4 caracteres')
+      .max(8, 'A senha deve ter no máximo 8 caracteres')
+      .required('O campo senha é obrigatório'),
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: schema,
+    onSubmit: values => {
+      console.log('Dados enviados:', values)
+      router.push('/dashboard')
+    },
+  })
+
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <div className="w-96 flex flex-col">
@@ -31,14 +62,21 @@ const SignIn = () => {
           </Social.Root>
         </div>
 
-        <form className="w-full mt-10 space-y-4">
+        <form onSubmit={formik.handleSubmit} className="w-full mt-10 space-y-4">
           <Root.FormRoot>
             <Root.Label>Your email</Root.Label>
             <Root.InputFild
               type="email"
               id="email"
-              placeholder="Pleace insert your e-mail adress"
+              name="email"
+              placeholder="Please insert your e-mail address"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-red-500 text-sm">{formik.errors.email}</div>
+            ) : null}
           </Root.FormRoot>
 
           <Root.FormRoot>
@@ -46,15 +84,22 @@ const SignIn = () => {
             <Root.InputFild
               type="password"
               id="password"
-              placeholder="Pleace insert your password adress"
+              name="password"
+              placeholder="Please insert your password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.password && formik.errors.password ? (
+              <div className="text-red-500 text-sm">
+                {formik.errors.password}
+              </div>
+            ) : null}
           </Root.FormRoot>
 
-          <Link href="/dashboard">
-            <Button type="button" className="w-full mt-3.5">
-              Sign in
-            </Button>
-          </Link>
+          <Button type="submit" className="w-full mt-3.5">
+            Sign in
+          </Button>
 
           <p className="text-center text-sm text-white cursor-pointer hover:underline">
             Não tens uma conta? Criar conta agora.
